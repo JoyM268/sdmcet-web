6c2a4c045ef7206d29d4a502014a15b9d2cf56b2
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-04-15 20:15:56 +0530

    JavaScript

---
+++ Unit - 3/MyFIrstJSWebpage.js
@@ -0,0 +1,1 @@
+document.write("SDM College of Engineering");
---
+++ Unit - 3/PopupWindow.js
@@ -0,0 +1,3 @@
+alert("Are you sure!! you want delete the file!!!");
+confirm("Do you want to submit the Data to the Server!!!!");
+prompt("Enter your name!!", "");
