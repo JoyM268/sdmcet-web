5b43477a1445c9f671dd06989e61625f8f1a5dbc
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-04-25 16:45:19 +0530

    Update File

--- Unit - 3/MyFIrstJSWebpage.js
+++ Unit - 3/MyFIrstJSWebpage.js
@@ -1,1 +1,2 @@
+//instruction to write on webpage
 document.write("SDM College of Engineering");
--- Unit - 3/PopupWindow.js
+++ Unit - 3/PopupWindow.js
@@ -1,3 +1,4 @@
+//Various popup windows in js
 alert("Are you sure!! you want delete the file!!!");
 confirm("Do you want to submit the Data to the Server!!!!");
 prompt("Enter your name!!", "");
