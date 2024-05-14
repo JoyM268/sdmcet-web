d52a4612f1206ba74ce25a992618d0290b7a9c86
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-05-14 12:47:09 +0530

    Add File

---
+++ Unit - 4/MyFirstPHPProg.php
@@ -0,0 +1,18 @@
+<?php
+    echo "<h3>Print instructions in PHP</h3>"."<br/>";
+    echo "Hello!! Welcome to the PHP world"."<br/>";
+    printf("Message from print function");
+    printf("Message from printf function");
+
+    echo"<hr>";
+    echo "<h3>Variables in PHP</h3>";
+    $num = 200;
+    $str = "SDM college";
+    $fnum = 30.65;
+
+    echo"First varialble value is: ".$num."<br/>";
+    echo"Second varialble value is: ".$str."<br/>";
+    echo"Third varialble value is: ".$fnum."<br/>";
+
+    echo date("y-m-d");
+?>
