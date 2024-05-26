a81b5e168226637bb1fcb2962a95c7ea848b176c
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-05-26 14:40:09 +0530

    Add File

--- Unit - 4/FunctionsInPHP.php
+++ Unit - 4/FunctionsInPHP.php
@@ -1,8 +1,29 @@
 <?php
     //Program to demonstrate user defined functions in PHP
-    greetUser();
+    $str1 = "sdm";
+    $str2 = "cet";
+    $n1 = $n2 = 40;
 
+    //Without Paremeter
     function greetUser(){
-        echo "Welcome to functions in PHP";
+        echo "Welcome to functions in PHP<br/>";
     }
+    
+
+    //With String parameter 
+    function concatStrings($s1, $s2){
+        echo "Concatenate strings: ".$s1.$s2."<br/>";
+        echo "Length of the first string: ".strlen($s1)."<br/>";
+        echo "Length of the second string: ".strlen($s2)."<br/>";
+    }
+
+    //With integer parameter
+    function exponentialValue($x, $y){
+        return $x ** $y;
+    }
+
+    greetUser();
+    concatStrings($str1, $str2);
+    $exp = exponentialValue($n1, $n2);
+    echo "Exponential result is: ".$exp;
 ?>
---
+++ Unit - 4/GreetUsers.php
@@ -0,0 +1,4 @@
+<?php
+    $usrName = $_GET["txtname"];
+    echo "Hello!!!! <b><u>$usrName</u></b> GOOD MORNING, WELCOME TO WEB WORLD";
+?>
